80715ba5e58d3a795f3f9f7ef01241cc979f04e3
ArnavU <arnavumarkar3@gmail.com>
2024-02-07 15:07:54 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Server is ready");
+});
+
+app.get("/jokes", (req, res)=>{
+    const jokes = [
+        {
+            id: 1,
+            title: "A joke",
+            content: "This is a joke",
+        },
+        {
+            id: 2,
+            title: "Another joke",
+            content: "This is a joke",
+        },
+        {
+            id: 3,
+            title: "A third joke",
+            content: "This is a joke",
+        },
+        {
+            id: 4,
+            title: "A fourth joke",
+            content: "This is a joke",
+        },
+        {
+            id: 5,
+            title: "A fifth joke",
+            content: "This is a joke",
+        },
+        {
+            id: 6,
+            title: "A sixth joke",
+            content: "This is a joke",
+        },
+    ]
+
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Server is running at localhost:${port}`);
+});
